9953a509507e6f5097b46bb02c2aef7f4577f523
Kouji Matsui <k@kekyo.net>
2018-12-18 10:36:17 +0900

    Update version.

--- CenterCLR.RelaxVersioner/Properties/AssemblyInfo.cs
+++ CenterCLR.RelaxVersioner/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("0b01c9d5-a685-4444-981b-e8b4d6bdbbf1")]
 
 // TODO: Will be removed if stable self.
-[assembly: AssemblyVersion("0.8.11.0")]
-[assembly: AssemblyFileVersion("0.8.11.0")]
+[assembly: AssemblyVersion("0.8.20.0")]
+[assembly: AssemblyFileVersion("0.8.20.0")]
