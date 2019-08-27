89028cca9e988f6fcd74ef816405cd39f51eae79
Kouji Matsui <k@kekyo.net>
2019-08-27 21:46:10 +0900

    Updated self host and test projects version.

--- TestProjects/CPlusPlusTestProject/RelaxVersioner.cpp
+++ TestProjects/CPlusPlusTestProject/RelaxVersioner.cpp
@@ -1,6 +1,6 @@
-// This is auto-generated version information attributes by CenterCLR.RelaxVersioner.0.9.60.0
+// This is auto-generated version information attributes by CenterCLR.RelaxVersioner.0.9.61.0
 // Do not edit.
-// Generated date: Tue, 27 Aug 2019 12:36:58 GMT
+// Generated date: Tue, 27 Aug 2019 12:44:48 GMT
 
 #include "stdafx.h"
 
@@ -26,12 +26,12 @@ namespace System
 using namespace System::Reflection;
 using namespace System::Runtime::InteropServices;
 
-[assembly: AssemblyVersionAttribute("0.9.60")];
-[assembly: AssemblyFileVersionAttribute("2019.8.27.38855")];
-[assembly: AssemblyInformationalVersionAttribute("1670ad556fd8481acffc1e263179e67e2bcbbb44")];
-[assembly: AssemblyVersionMetadataAttribute("Build","Tue, 27 Aug 2019 12:35:10 GMT")];
+[assembly: AssemblyVersionAttribute("0.9.61")];
+[assembly: AssemblyFileVersionAttribute("2019.8.27.39100")];
+[assembly: AssemblyInformationalVersionAttribute("b3e2eb866fc050bdff62c0206c142632df9e6d64")];
+[assembly: AssemblyVersionMetadataAttribute("Build","Tue, 27 Aug 2019 12:43:20 GMT")];
 [assembly: AssemblyVersionMetadataAttribute("Branch","")];
-[assembly: AssemblyVersionMetadataAttribute("Tags","0.9.60")];
+[assembly: AssemblyVersionMetadataAttribute("Tags","0.9.61")];
 [assembly: AssemblyVersionMetadataAttribute("Author","Kouji Matsui <k@kekyo.net>")];
 [assembly: AssemblyVersionMetadataAttribute("Committer","Kouji Matsui <k@kekyo.net>")];
 [assembly: AssemblyVersionMetadataAttribute("Message","wip")];
