7bafd238b7933e2aafb75351079d10976c956d06
Kouji Matsui <k@kekyo.net>
2019-09-12 22:55:48 +0900

    Updated self host and test projects version.

--- TestProjects/CPlusPlusTestProject/RelaxVersioner.cpp
+++ TestProjects/CPlusPlusTestProject/RelaxVersioner.cpp
@@ -1,5 +1,5 @@
-// This is auto-generated version information attributes by CenterCLR.RelaxVersioner.0.10.52.0, Do not edit.
-// Generated date: Thu, 12 Sep 2019 13:30:11 GMT
+// This is auto-generated version information attributes by CenterCLR.RelaxVersioner.1.0.0.0, Do not edit.
+// Generated date: Thu, 12 Sep 2019 13:54:47 GMT
 
 #include "stdafx.h"
 
@@ -24,15 +24,15 @@ namespace System
 
 using namespace System::Reflection;
 
-[assembly: AssemblyVersionAttribute("0.10.52")];
-[assembly: AssemblyFileVersionAttribute("2019.9.12.40398")];
-[assembly: AssemblyInformationalVersionAttribute("0e8b85ffb2921e0ed3738e122e2859290181315a")];
-[assembly: AssemblyVersionMetadataAttribute("Date","Thu, 12 Sep 2019 13:26:36 GMT")];
-[assembly: AssemblyVersionMetadataAttribute("Branch","devel")];
-[assembly: AssemblyVersionMetadataAttribute("Tags","")];
+[assembly: AssemblyVersionAttribute("1.0.0")];
+[assembly: AssemblyFileVersionAttribute("2019.9.12.40881")];
+[assembly: AssemblyInformationalVersionAttribute("967c229a68a251fade829e789284cbc5848b1398")];
+[assembly: AssemblyVersionMetadataAttribute("Date","Thu, 12 Sep 2019 13:42:42 GMT")];
+[assembly: AssemblyVersionMetadataAttribute("Branch","master")];
+[assembly: AssemblyVersionMetadataAttribute("Tags","1.0.0")];
 [assembly: AssemblyVersionMetadataAttribute("Author","Kouji Matsui <k@kekyo.net>")];
 [assembly: AssemblyVersionMetadataAttribute("Committer","Kouji Matsui <k@kekyo.net>")];
-[assembly: AssemblyVersionMetadataAttribute("Message","Updated self host and test projects version.")];
+[assembly: AssemblyVersionMetadataAttribute("Message","Merge branch 'devel'")];
 [assembly: AssemblyVersionMetadataAttribute("Build","")];
-[assembly: AssemblyVersionMetadataAttribute("Generated","Thu, 12 Sep 2019 13:30:11 GMT")];
+[assembly: AssemblyVersionMetadataAttribute("Generated","Thu, 12 Sep 2019 13:54:47 GMT")];
 
